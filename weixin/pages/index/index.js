7a76232936d77a32812a7f3be39c0fe0bec311f3
KingBright <jl.kingbright@gmail.com>
2017-04-13 02:13:41 +0800

    add bind page

--- weixin/pages/index/index.js
+++ weixin/pages/index/index.js
@@ -3,14 +3,14 @@
 var app = getApp()
 Page({
   data: {
-    motto: '您尚未绑定手机号',
+    message: '您尚未绑定手机号',
     button: '立即去绑定',
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
+  toBind: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../bind/bind'
     })
   },
   onLoad: function () {
@@ -19,6 +19,7 @@ Page({
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
+      console.log(userInfo)
       that.setData({
         userInfo:userInfo
       })
